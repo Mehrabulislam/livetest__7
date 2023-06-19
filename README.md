a2f52045d2f33661f503947ff2972f342c82b59b
Mehrabul islam <mehrabulislam14@gmail.com>
2023-06-19 23:38:02 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# livetest_7
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+![](../Downloads/355048677_989557182090499_3033378679026934627_n.jpg)
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      if (_counter == 5) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Counter value is 5!'),
+              actions: [
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      } else if (_counter == 10) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SecondScreen(),
+          ),
+        );
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Counter App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Counter Value: $_counter',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: ElevatedButton.icon(
+                      onPressed: _incrementCounter,
+                      icon: Icon(Icons.add),
+                      label: Text(''),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Flexible(
+                    flex: 2,
+                    child: ElevatedButton.icon(
+                      onPressed: _decrementCounter,
+                      icon: Icon(Icons.remove),
+                      label: Text(''),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Congratulations! You reached 10!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
